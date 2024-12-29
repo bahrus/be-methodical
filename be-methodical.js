bcf8f6a86946f820f3a455cf27b4061ffdd7360c
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-12-28 19:50:30 -0500

    Initial commit

--- be-methodical.js
+++ be-methodical.js
@@ -2,3 +2,7 @@
 import { BE } from 'be-enhanced/BE.js';
 import { propInfo, resolved, rejected } from 'be-enhanced/cc.js';
 import { dispatchEvent as de } from 'trans-render/positractions/dispatchEvent.js';
+
+/** @import {BEConfig, IEnhancement, BEAllProps} from './ts-refs/be-enhanced/types.d.ts' */
+/** @import {Actions, PAP, AP, BAP, ObservingParameters} from './ts-refs/be-methodical/types' */
+
