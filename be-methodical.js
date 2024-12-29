cdb24f3da1b592c97b8d2c589aa7ef1124306a95
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-12-28 20:11:57 -0500

    Initial commit

--- be-methodical.js
+++ be-methodical.js
@@ -4,5 +4,19 @@ import { propInfo, resolved, rejected } from 'be-enhanced/cc.js';
 import { dispatchEvent as de } from 'trans-render/positractions/dispatchEvent.js';
 
 /** @import {BEConfig, IEnhancement, BEAllProps} from './ts-refs/be-enhanced/types.d.ts' */
-/** @import {Actions, PAP, AP, BAP, ObservingParameters} from './ts-refs/be-methodical/types' */
+/** @import {Actions, PAP, AP, BAP, ObservingParameters} from './ts-refs/be-methodical/types.d.ts' */
 
+class BeMethodical extends BE {
+    de = de;
+    /**
+     * @type {BEConfig<BAP, Actions & IEnhancement, any>}
+     */
+    static config = {
+        propInfo: {
+            ...propInfo,
+        }
+    };
+}
+
+await BeMethodical.bootUp();
+export {BeMethodical};
---
+++ emc.js
@@ -0,0 +1,20 @@
+// @ts-check
+import { BeHive, seed, MountObserver } from 'be-hive/be-hive.js';
+/** @import {EMC, EventListenerOrFn} from './ts-refs/trans-render/be/types' */
+/** @import {Actions, PAP,  AP} from './ts-refs/be-methodical/types' */;
+
+/**
+ * @type {Partial<EMC<any, AP>>}
+ */
+export const emc = {
+    enhancedElementMatches: 'script[nomodule]',
+    base: 'be-methodical',
+    map: {
+        '0.0':{
+            instanceOf: 'String',
+            mapsTo: 'ceName'
+        }
+    }
+};
+const mose = seed(emc);
+MountObserver.synthesize(document, BeHive, mose);
